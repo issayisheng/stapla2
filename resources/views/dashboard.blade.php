1ccdea2eabf9b5d465bd0bf30d4a609690486f3a
nishida <k.nishida@vistage-llc.com>
2020-08-29 22:55:48 +0900

    ダッシュボード変更

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="col-md-12">
+        <div class="col-lg-12">
             <div class="card">
                 <div class="card-header">ジムを予約する</div>
                 <div class="card-body">
@@ -12,29 +12,35 @@
                         {{ session('status') }}
                     </div>
                     @endif
-                    <div class="card-deck">
-                        <div class="card">
-                            <img class="card-img-top" src="img/gym1.jpg" alt="PALプライベートジム学芸大学">
-                            <div class="card-body">
-                                <h5 class="card-title">PALプライベートジム学芸大学</h5>
-                                <p class="card-text">東急東横線学芸大学駅徒歩２分に位置するパーソナルジム</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                    <div class="card-group">
+                        <div class="col-sm-12 col-md-6 col-lg-4 px-0 px-md-3">
+                            <div class="card mb-3 mb-lg-0">
+                                <img class="card-img-top" src="img/gym1.jpg" alt="PALプライベートジム学芸大学">
+                                <div class="card-body">
+                                    <h5 class="card-title">PALプライベートジム学芸大学</h5>
+                                    <p class="card-text">東急東横線学芸大学駅徒歩２分に位置するパーソナルジム</p>
+                                    <div class="text-center"><a href="#" class="btn btn-primary">予約する</a></div>
+                                </div>
                             </div>
                         </div>
-                        <div class="card">
-                            <img class="card-img-top" src="img/gym2.jpg" alt="PALプライベートジム烏丸御池">
-                            <div class="card-body">
-                                <h5 class="card-title">PALプライベートジム烏丸御池</h5>
-                                <p class="card-text">京都市営地下鉄烏丸御池駅より徒歩5分に位置するパーソナルジム</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <div class="col-sm-12 col-md-6 col-lg-4 px-0 px-md-3">
+                            <div class="card mb-3 mb-lg-0">
+                                <img class="card-img-top" src="img/gym2.jpg" alt="PALプライベートジム烏丸御池">
+                                <div class="card-body">
+                                    <h5 class="card-title">PALプライベートジム烏丸御池</h5>
+                                    <p class="card-text">京都市営地下鉄烏丸御池駅より徒歩5分に位置するパーソナルジム</p>
+                                    <div class="text-center"><a href="#" class="btn btn-success">予約する</a></div>
+                                </div>
                             </div>
                         </div>
-                        <div class="card">
-                            <img class="card-img-top" src="img/gym3.jpg" alt="PALプライベートジム豊中">
-                            <div class="card-body">
-                                <h5 class="card-title">PALプライベートジム豊中</h5>
-                                <p class="card-text">大阪モノレール柴原阪大前駅より徒歩12分に位置するプライベートジム</p>
-                                <a href="#" class="btn btn-primary">Go somewhere</a>
+                        <div class="col-sm-12 col-md-6 col-lg-4 px-0 px-md-3">
+                            <div class="card mb-3 mb-lg-0">
+                                <img class="card-img-top" src="img/gym3.jpg" alt="PALプライベートジム豊中">
+                                <div class="card-body">
+                                    <h5 class="card-title">PALプライベートジム豊中</h5>
+                                    <p class="card-text">大阪モノレール柴原阪大前駅より徒歩12分に位置するプライベートジム</p>
+                                    <div class="text-center"><a href="#" class="btn btn-danger">予約する</a></div>
+                                </div>
                             </div>
                         </div>
                     </div>
