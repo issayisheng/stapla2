e3e4c70269ba03bc9dac174954f1282528d97278
nishida <k.nishida@vistage-llc.com>
2020-08-29 22:57:44 +0900

    新規登録、ログイン設定変更

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,46 +1,35 @@
-@extends('layouts.app')
+@extends('layouts.auth.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Reset Password') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    <form method="POST" action="{{ route('password.email') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Send Password Reset Link') }}
-                                </button>
-                            </div>
-                        </div>
-                    </form>
-                </div>
+<div class="form password-reset">
+    <div class="container">
+        <div class="form-logo"><a href="{{route('top')}}" class="form-logo__link">Stapla</a></div>
+        <div class="form-main">
+            <h1 class="form-main__title">パスワード再設定</h1>
+            @if (session('status'))
+            <div class="alert alert-success" role="alert">
+                {{ session('status') }}
             </div>
+            @endif
+            <form class="layout-form" method="POST" action="{{ route('password.email') }}">
+                @csrf
+                <div class="form-group">
+                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" autocomplete="email" autofocus placeholder="メールアドレスを入力">
+                    <p class="password-reset__info">パスワード再設定のためのご案内メールを、ご登録されたメールアドレスに送信されます。</p>
+                    @error('email')
+                    <span class="invalid-feedback" role="alert">
+                        <strong>{{ $message }}</strong>
+                    </span>
+                    @enderror
+                </div>
+
+                <div class="form-group text-center mb-0">
+                    <button type="submit" class="btn-default btn-default--mail">
+                        送信する
+                    </button>
+                </div>
+            </form>
+            <a href="{{ route('login') }}" class="password-reset__link">ログイン画面に戻る</a>
         </div>
     </div>
 </div>
