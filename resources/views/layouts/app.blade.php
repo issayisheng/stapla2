28811ff42c9b2cc94c97b7a5956de006df11620c
nishida <k.nishida@vistage-llc.com>
2020-09-06 10:19:25 +0900

    ナビゲーションメニュー変更

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -60,7 +60,11 @@
                             </a>
                             <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
                                 <a class="dropdown-item" href="{{ route('user_info.index') }}">マイページ</a>
-                                <a class="dropdown-item" href="{{ route('user_info.index') }}">ジム登録</a>
+                                @if( Auth::user()->status === null || Auth::user()->status === 0 )
+                                <a class="dropdown-item" href="{{ route('gym.create') }}">ジム登録</a>
+                                @else
+                                <a class="dropdown-item" href="{{ route('gym.index') }}">ジム情報</a>
+                                @endif
                                 <a class="dropdown-item" href="{{ route('reserved.index') }}">予約状況</a>
                                 <a class="dropdown-item" href="{{ route('ticket.index') }}">チケット購入</a>
                                 <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault();
