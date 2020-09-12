cf581d1bad3eadbe90bc4abadddf11766de92bfe
Ken1roKomatsu <ken1ro.komatsu@gmail.com>
2020-09-12 15:39:44 +0900

    display gym info and do constantization

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,36 +13,18 @@
                     </div>
                     @endif
                     <div class="card-group">
+                        @foreach($gyms as $gym)
                         <div class="col-sm-12 col-md-6 col-lg-4 px-0 px-md-3">
                             <div class="card mb-3 mb-lg-0">
                                 <img class="card-img-top" src="img/gym1.jpg" alt="PALプライベートジム学芸大学">
                                 <div class="card-body">
-                                    <h5 class="card-title">PALプライベートジム学芸大学</h5>
+                                    <h5 class="card-title"><a href="{{ route('gym.show', ['gym_id'=>$gym->id]) }}">{{ $gym->name }}</a></h5>
                                     <p class="card-text">東急東横線学芸大学駅徒歩２分に位置するパーソナルジム</p>
                                     <div class="text-center"><a href="#" class="btn btn-primary">予約する</a></div>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-sm-12 col-md-6 col-lg-4 px-0 px-md-3">
-                            <div class="card mb-3 mb-lg-0">
-                                <img class="card-img-top" src="img/gym2.jpg" alt="PALプライベートジム烏丸御池">
-                                <div class="card-body">
-                                    <h5 class="card-title">PALプライベートジム烏丸御池</h5>
-                                    <p class="card-text">京都市営地下鉄烏丸御池駅より徒歩5分に位置するパーソナルジム</p>
-                                    <div class="text-center"><a href="#" class="btn btn-success">予約する</a></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-sm-12 col-md-6 col-lg-4 px-0 px-md-3">
-                            <div class="card mb-3 mb-lg-0">
-                                <img class="card-img-top" src="img/gym3.jpg" alt="PALプライベートジム豊中">
-                                <div class="card-body">
-                                    <h5 class="card-title">PALプライベートジム豊中</h5>
-                                    <p class="card-text">大阪モノレール柴原阪大前駅より徒歩12分に位置するプライベートジム</p>
-                                    <div class="text-center"><a href="#" class="btn btn-danger">予約する</a></div>
-                                </div>
-                            </div>
-                        </div>
+                        @endforeach
                     </div>
                 </div>
             </div>
