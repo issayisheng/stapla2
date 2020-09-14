c3f38c3e5eca6ae4a8d2181e27dee3cbe060e6e3
nishida <k.nishida@vistage-llc.com>
2020-09-14 16:15:52 +0900

    ダッシュボード変更

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -13,36 +13,25 @@
                     </div>
                     @endif
                     <div class="card-group">
+                        @foreach($gyms as $gym)
                         <div class="col-sm-12 col-md-6 col-lg-4 px-0 px-md-3">
-                            <div class="card mb-3 mb-lg-0">
-                                <img class="card-img-top" src="img/gym1.jpg" alt="PALプライベートジム学芸大学">
+                            <div class="card mb-3">
+                                @if($gym->gym_image == null)
+                                <img class="card-img-top" src="/public/gym/noimage.png">
+                                @else
+                                <img class="card-img-top" src="{{ env('AWS_URL').$gym->introduction_pic }}" alt="{{ $gym->name }}">
+                                @endif
                                 <div class="card-body">
-                                    <h5 class="card-title">PALプライベートジム学芸大学</h5>
-                                    <p class="card-text">東急東横線学芸大学駅徒歩２分に位置するパーソナルジム</p>
-                                    <div class="text-center"><a href="#" class="btn btn-primary">予約する</a></div>
+                                    <h5 class="card-title">{{ $gym->name }}</h5>
+                                    <p class="card-text">{{ $gym->introduction_text }}</p>
+                                    <div class="d-flex justify-content-center">
+                                        <a href="{{ route('gym.show', ['gym_id'=>$gym->id]) }}" class="btn page-link text-dark d-inline-block mr-3">詳細をみる</a>
+                                        <a href="#" class="btn btn-danger">予約する</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-sm-12 col-md-6 col-lg-4 px-0 px-md-3">
-                            <div class="card mb-3 mb-lg-0">
-                                <img class="card-img-top" src="img/gym2.jpg" alt="PALプライベートジム烏丸御池">
-                                <div class="card-body">
-                                    <h5 class="card-title">PALプライベートジム烏丸御池</h5>
-                                    <p class="card-text">京都市営地下鉄烏丸御池駅より徒歩5分に位置するパーソナルジム</p>
-                                    <div class="text-center"><a href="#" class="btn btn-success">予約する</a></div>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-sm-12 col-md-6 col-lg-4 px-0 px-md-3">
-                            <div class="card mb-3 mb-lg-0">
-                                <img class="card-img-top" src="img/gym3.jpg" alt="PALプライベートジム豊中">
-                                <div class="card-body">
-                                    <h5 class="card-title">PALプライベートジム豊中</h5>
-                                    <p class="card-text">大阪モノレール柴原阪大前駅より徒歩12分に位置するプライベートジム</p>
-                                    <div class="text-center"><a href="#" class="btn btn-danger">予約する</a></div>
-                                </div>
-                            </div>
-                        </div>
+                        @endforeach
                     </div>
                 </div>
             </div>
