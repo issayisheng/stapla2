25866d975259d05dd7c40474de8c3e3cef07c20d
nishida <k.nishida@vistage-llc.com>
2020-09-21 14:05:19 +0900

    Reservedをvueに移行（仮）

---
+++ resources/js/components/settings/reserved/Reserved.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="container py-5">
+        <div class="row justify-content-center">
+            <div class="col-md-10">
+                <div class="card">
+                    <div class="card-header">予約一覧</div>
+                    <div class="card-body">
+                        <p>現在、予約はありません</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
