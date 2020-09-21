2b64421f005cf4b46c223d2015ddb1dc228c797e
nishida <k.nishida@vistage-llc.com>
2020-09-21 14:07:13 +0900

    api.phpでルーティング設定

--- routes/api.php
+++ routes/api.php
@@ -18,12 +18,39 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 
-Route::get('/user_info', function (Request $request) {
-    return $users = Auth::user(); // ログイン中のユーザー情報
+Route::group(['prefix' => 'settings'], function () {
+    Route::apiResources([
+        'gym_info' => 'GymController',         // ジム設定
+        'user_info' => 'UserInfoController' ,  // ユーザー設定
+        'reserved'  => 'ReservedController',   // 予約情報
+        'history'   => 'HistoryController',    // チケット履歴
+        'ticket'    => 'TicketController',     // チケット残数
+        'trainer'   => 'TrainerController',    // トレーナー管理
+    ]);
+
+    // パスワード設定
+    Route::post('user_info/password', 'UserInfoPasswordController@store')->name('user_info.password.store');
+    Route::get('user_info/password/edit/{id}', 'UserInfoPasswordController@edit')->name('user_info.password.edit');
+    Route::post('user_info/password/update/{id}', 'UserInfoPasswordController@update')->name('user_info.password.update');
 });
+
+
+Route::apiResources([
+    'gym'       => 'MainGymController',         // ジム設定
+]);
+
+
+
+// Stripeの支払情報
+Route::post('/charge', 'ChargeController@charge')->name('charge');
+
+
+// 住所検索
+Route::get('/zipcode', 'GymController@getZipInfo');
+
 
 
 // Route::post('/login', 'Api\AuthController@login');
 
-
-Route::get('calendar', 'Api\CalendarController@index');
+// Route::post('/register', 'Auth\RegisterController@register')->name('register');  // 会員登録
+// Route::post('/login', 'Auth\LoginController@login')->name('login');             // ログイン
