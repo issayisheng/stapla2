99f2c3b2fddda29b491bb7dadd66bdaf24224775
nishida <k.nishida@vistage-llc.com>
2020-09-21 13:50:17 +0900

    top.blade.phpをvueへ移行

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -1,49 +1,17 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Stapla</title>
-    <!-- Fonts -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700&display=swap" rel="stylesheet">
-    <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <style>
-        html,
-        body {
-            height: 100vh;
-            margin: 0;
-        }
-
-        .full-height {
-            height: 100vh;
-        }
-
-        .flex-center {
-            align-items: center;
-            display: flex;
-            justify-content: center;
-        }
-
-        .content {
-            text-align: center;
-        }
-
-        .title {
-            font-size: 64px;
-        }
-    </style>
 </head>
 <body>
     <div id="app">
-        <div class="flex-center position-ref full-height">
-            <div class="content">
-                <h1 class="title mb-4">Stapla</h1>
-                <p class="mb-5">ここに説明文が入ります。<br>ここに説明文が入ります。</p>
-                <a class="btn btn-secondary mr-3 btn-lg" href="{{ route('register') }}" role="button">新規登録</a>
-                <a class="btn btn-dark btn-lg" href="{{ route('login') }}" role="button">ログイン</a>
-            </div>
-        </div>
+        <app-component></app-component>
     </div>
     <script src="{{ asset('js/app.js') }}" defer></script>
 </body>
