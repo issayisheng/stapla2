d56a3bd7f2a145cdd61a29f89bcf301545df36b4
nishida <k.nishida@vistage-llc.com>
2020-09-25 22:45:03 +0900

    Stripeの<script>タグ導入

--- app/Http/Controllers/ChargeController.php
+++ app/Http/Controllers/ChargeController.php
@@ -74,31 +74,4 @@ class ChargeController extends Controller
             return back()->with('errors', "決済に失敗しました。しばらく経ってから再度お試しください。");
         }
     }
-
-
-    // public function session()
-    // {
-    //     Stripe::setApiKey('sk_test_51HFuIFFuVe19htwq23yg5d6SAB07PlxqQK29938g0VnEvWf9a3BUBrJFwzKDU0lpjOcVXxpFEGQOJeb2VRukOAk100AnskWtEX');
-        
-    //     header('Content-Type: application/json');
-    
-    //     $checkout_session = \Stripe\Checkout\Session::create([
-    //         'payment_method_types' => ['card'],
-    //         'line_items' => [[
-    //             'price_data' => [
-    //                 'currency' => 'usd',
-    //                 'unit_amount' => 2000,
-    //                 'product_data' => [
-    //                     'name' => 'Stubborn Attachments',
-    //                     'images' => ["https://i.imgur.com/EHyR2nP.png"],
-    //                 ],
-    //             ],
-    //             'quantity' => 1,
-    //         ]],
-    //         'mode' => 'payment',
-    //         'success_url' => 'http://localhost.stapla.com/settings/ticket',
-    //         'cancel_url' => 'http://localhost.stapla.com/settings/reserved',
-    //     ]);
-    //     echo json_encode(['id' => $checkout_session->id]);
-    // }
 }
--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -8,6 +8,8 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700&display=swap" rel="stylesheet">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <script src="https://polyfill.io/v3/polyfill.min.js?version=3.52.1&features=fetch"></script>
+    <script src="https://js.stripe.com/v3/"></script>
 </head>
 <body>
     <div id="app">
