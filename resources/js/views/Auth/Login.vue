0463b87a3c877dadfb4513cc9385b1ab4f4d95f4
Kiyoshi Nishida <kiyoshinishida@MacBook-Pro.local>
2020-11-11 15:25:03 +0900

    Login,Registerのレイアウト変更

--- resources/js/views/Auth/Login.vue
+++ resources/js/views/Auth/Login.vue
@@ -1,89 +1,100 @@
 <template>
-    <div class="login form">
+    <div>
+        <login-header></login-header>
         <div class="container">
-            <div class="form-logo">
-                <router-link to="/" class="form-logo__link">Stapla</router-link>
-            </div>
             <div class="form-main">
                 <h1 class="form-main__title">ログイン</h1>
-                <div v-if="message" class="alert alert-danger">
-                    {{ message }}
-                </div>
-                <form class="layout-form" @submit.prevent="submit">
-                    <div class="form-group">
-                        <input
-                            id="email"
-                            type="email"
-                            class="form-control"
-                            :class="{
-                                'is-invalid': errors.email
-                            }"
-                            name="email"
-                            placeholder="メールアドレスを入力"
-                            v-model="form.email"
-                            @change="onChangeEmail"
-                        />
-                        <span
-                            class="invalid-feedback"
-                            role="alert"
-                            v-if="errors.email"
-                        >
-                            <strong v-text="errors.email"></strong
-                        ></span>
+                <div class="form-main__content">
+                    <div v-if="message" class="alert alert-danger">
+                        {{ message }}
                     </div>
-                    <div class="form-group">
-                        <input
-                            id="password"
-                            type="password"
-                            class="form-control"
-                            :class="{
-                                'is-invalid': errors.password
-                            }"
-                            name="password"
-                            placeholder="パスワードを入力"
-                            v-model="form.password"
-                            @change="onChangePassword"
-                        />
-                        <span
-                            class="invalid-feedback"
-                            role="alert"
-                            v-if="errors.password"
+                    <form class="login-form" @submit.prevent="submit">
+                        <div class="form-group">
+                            <input
+                                id="email"
+                                type="email"
+                                class="form-control"
+                                :class="{
+                                    'is-invalid': errors.email
+                                }"
+                                name="email"
+                                placeholder="メールアドレスを入力"
+                                v-model="form.email"
+                                @change="onChangeEmail"
+                            />
+                            <span
+                                class="invalid-feedback"
+                                role="alert"
+                                v-if="errors.email"
+                            >
+                                <strong v-text="errors.email"></strong
+                            ></span>
+                        </div>
+                        <div class="form-group">
+                            <input
+                                id="password"
+                                type="password"
+                                class="form-control"
+                                :class="{
+                                    'is-invalid': errors.password
+                                }"
+                                name="password"
+                                placeholder="パスワードを入力"
+                                v-model="form.password"
+                                @change="onChangePassword"
+                            />
+                            <span
+                                class="invalid-feedback"
+                                role="alert"
+                                v-if="errors.password"
+                            >
+                                <strong v-text="errors.password"></strong
+                            ></span>
+                        </div>
+                        <button
+                            type="submit"
+                            class="btn-default btn-default--mail"
                         >
-                            <strong v-text="errors.password"></strong
-                        ></span>
+                            ログイン
+                        </button>
+                        <div>
+                            <router-link to="/register" class="form-main__link"
+                                >新規登録の方</router-link
+                            >
+                            <router-link
+                                to="password/email"
+                                class="form-main__link"
+                                >パスワードを忘れた方はこちら</router-link
+                            >
+                        </div>
+                    </form>
+                    <div class="social-form">
+                        <p class="social-form__deco">または</p>
+                        <a
+                            class="btn-default btn-default--facebook"
+                            href="/login/facebook"
+                        >
+                            <img
+                                :src="'/img/common/facebook-logo.svg'"
+                                alt="Facebook"
+                                class="btn-default__logo"
+                            />Facebookでログイン
+                        </a>
+                        <a
+                            class="btn-default btn-default--google"
+                            href="/login/google"
+                        >
+                            <img
+                                :src="'/img/common/google-logo.svg'"
+                                alt="Google"
+                                class="btn-default__logo"
+                            />Googleでログイン
+                        </a>
                     </div>
-                    <button type="submit" class="btn-default btn-default--mail">
-                        ログイン
-                    </button>
-                    <router-link to="password/email" class="form__link"
-                        >パスワードを忘れた方はこちら</router-link
-                    >
-                </form>
-                <div class="social-form">
-                    <p class="social-form__deco">または</p>
-                    <a
-                        class="btn-default btn-default--facebook"
-                        href="/auth/login/facebook"
-                    >
-                        <img
-                            :src="'/img/facebook-logo.svg'"
-                            alt="Facebook"
-                            class="btn-default__logo"
-                        />Facebookでログイン
-                    </a>
-                    <a
-                        class="btn-default btn-default--google"
-                        href="api/auth/login/google"
-                    >
-                        <img
-                            :src="'/img/google-logo.svg'"
-                            alt="Google"
-                            class="btn-default__logo"
-                        />Googleでログイン
-                    </a>
                 </div>
             </div>
         </div>
+        <login-footer></login-footer>
     </div>
 </template>
 
@@ -113,6 +124,9 @@ export default {
                 .then(() => {
                     this.$router.replace({
                         name: "dashboard"
+                    });
+                    this.flash("ログインしました", "success", {
+                        timeout: 3000
                     });
                 })
                 .catch(error => {
@@ -154,56 +168,31 @@ export default {
 </script>
 
 <style>
-.form {
-    padding: 45px 0 70px;
-}
-
-.form-logo {
-    text-align: center;
-    margin: 0 0 40px;
-    font-size: 30px;
-}
-
-.form-logo__link {
-    color: #333;
-}
-
-.form-logo__link:hover {
-    text-decoration: none;
-}
-
 .form-main {
     display: flex;
     flex-direction: column;
-    width: 100%;
-    padding: 32px;
-    margin: 0 auto;
-    background-color: #ffffff;
-    border: 1px solid #d6d6d6;
-    border-radius: 4px;
-}
-
-@media screen and (min-width: 768px) {
-    .form-main {
-        width: 400px;
-    }
+    background-color: #fff;
+    border-radius: 2px;
 }
 
 .form-main__title {
     font-size: 20px;
     text-align: center;
-    margin: 0 0 20px;
+    margin: 0 0 30px;
     font-weight: 700;
+    padding: 24px 20px;
+    border-bottom: solid 2px #f2f2f2;
 }
 
-.layout-form {
-    max-width: 400px;
-    margin: 0 auto;
+.form-main__content {
+    max-width: 380px;
+    margin: 0 auto 40px;
+    padding: 0 16px;
     width: 100%;
 }
 
-.form__desc {
-    margin: 0 0 20px;
+.login-form {
+    margin: 0 0 30px;
 }
 
 .form-group {
@@ -215,22 +204,14 @@ export default {
     height: auto;
 }
 
-.form__link {
+.form-main__link {
     color: #00a1e9;
-}
-
-.form__link:hover {
-    opacity: 0.8;
-}
-
-.login .form__link {
-    margin: 20px 0 0;
     display: block;
-    text-align: center;
+    margin: 0 0 10px;
 }
 
-.social-form {
-    margin: 32px 0 0;
+.form-desc__link {
+    color: #00a1e9;
 }
 
 .social-form__deco {
@@ -257,36 +238,20 @@ export default {
     margin-left: 2.5em;
 }
 
-.password-reset__link {
-    color: #00a1e9;
-    padding: 16px 0 0;
-    text-align: center;
-    border-top: 1px solid rgb(213, 216, 222);
-}
-
-.password-reset__info {
-    font-size: 12px;
-    color: #7a7a7a;
-    margin: 10px 0 0;
-    line-height: 1.5;
-}
-
 .btn-default {
     display: block;
     width: 100%;
     font-size: 14px;
     border-radius: 3px;
     border: none;
+    height: 48px;
     cursor: pointer;
     text-align: center;
     padding: 12px 0;
     position: relative;
     outline: none;
     transition: opacity 0.2s;
-}
-
-.btn-default:hover {
-    opacity: 0.8;
+    margin: 0 0 16px;
 }
 
 .btn-default:focus {
@@ -294,20 +259,20 @@ export default {
 }
 
 .btn-default--mail {
-    background: #ef1d1d;
+    background: #f17436;
     color: #fff;
 }
 
 .btn-default--facebook {
     background: #3b5998;
     color: #fff;
-    margin: 0 0 16px;
 }
 
 .btn-default--google {
     background: #fff;
     color: rgba(0, 0, 0, 0.54);
     border: 1px solid #ced4da;
+    margin: 0;
 }
 
 .btn-default__logo {
@@ -318,5 +283,28 @@ export default {
     top: 0;
     bottom: 0;
     margin: auto;
+}
+
+@media screen and (min-width: 768px) {
+    .form-main {
+        width: 650px;
+        margin: 0 auto;
+    }
+
+    .form-main__title {
+        margin: 0 0 40px;
+    }
+
+    .form-main__link:hover {
+        text-decoration: underline;
+    }
+
+    .form-desc__link:hover {
+        text-decoration: underline;
+    }
+
+    .btn-default:hover {
+        opacity: 0.8;
+    }
 }
 </style>
--- resources/js/views/Auth/Register.vue
+++ resources/js/views/Auth/Register.vue
@@ -1,217 +1,66 @@
 <template>
-    <div class="signup form">
+    <div>
+        <login-header></login-header>
         <div class="container">
-            <div class="form-logo">
-                <router-link to="/" class="form-logo__link">Stapla</router-link>
-            </div>
             <div class="form-main">
                 <h1 class="form-main__title">新規登録</h1>
-                <form @submit.prevent="signup">
-                    <div class="form-group">
-                        <label for="name" class="font-weight-bold"
-                            >お名前</label
+                <div class="form-main__content">
+                    <div class="form-btn__list">
+                        <router-link
+                            class="btn-default btn-default--mail"
+                            to="/register/email"
                         >
-                        <input
-                            id="name"
-                            type="text"
-                            name="name"
-                            class="form-control"
-                            :class="{
-                                'is-invalid': errors.name
-                            }"
-                            autofocus
-                            placeholder="スタプラ太郎"
-                            v-model="form.name"
-                            @change="onChangeName"
-                        />
-                        <span
-                            class="invalid-feedback"
-                            role="alert"
-                            v-if="errors.name"
+                            <img
+                                :src="'/img/common/mail.svg'"
+                                alt="Facebook"
+                                class="btn-default__logo"
+                            />
+                            メールアドレスで登録
+                        </router-link>
+                        <a
+                            class="btn-default btn-default--facebook"
+                            href="/login/facebook"
                         >
-                            <strong v-text="errors.name"></strong
-                        ></span>
+                            <img
+                                :src="'/img/common/facebook-logo.svg'"
+                                alt="Facebook"
+                                class="btn-default__logo"
+                            />Facebookで登録
+                        </a>
+                        <a
+                            class="btn-default btn-default--google"
+                            href="/login/google"
+                        >
+                            <img
+                                :src="'/img/common/google-logo.svg'"
+                                alt="Google"
+                                class="btn-default__logo"
+                            />Googleで登録
+                        </a>
                     </div>
-                    <div class="form-group">
-                        <label for="email" class="font-weight-bold"
-                            >メールアドレス</label
-                        >
-                        <input
-                            id="email"
-                            type="email"
-                            class="form-control"
-                            :class="{
-                                'is-invalid': errors.email
-                            }"
-                            name="email"
-                            placeholder="mail@stapla.net"
-                            v-model="form.email"
-                            @change="onChangeEmail"
-                        />
-                        <span
-                            class="invalid-feedback"
-                            role="alert"
-                            v-if="errors.email"
-                        >
-                            <strong v-text="errors.email"></strong
-                        ></span>
-                    </div>
-                    <div class="form-group">
-                        <label for="password" class="font-weight-bold"
-                            >パスワード</label
-                        >
-                        <input
-                            id="password"
-                            type="password"
-                            class="form-control"
-                            :class="{
-                                'is-invalid': errors.password
-                            }"
-                            name="password"
-                            placeholder="8文字以上の半角英数記号"
-                            v-model="form.password"
-                            @change="onChangePassword"
-                        />
-                        <span
-                            class="invalid-feedback"
-                            role="alert"
-                            v-if="errors.password"
-                        >
-                            <strong v-text="errors.password"></strong
-                        ></span>
-                    </div>
-                    <div class="form-group">
-                        <label
-                            for="password_confirmation"
-                            class="font-weight-bold"
-                            >パスワード確認</label
-                        >
-                        <input
-                            id="password_confirmation"
-                            type="password"
-                            class="form-control"
-                            :class="{
-                                'is-invalid': errors.password_confirmation
-                            }"
-                            name="password_confirmation"
-                            placeholder="パスワードを入力"
-                            v-model="form.password_confirmation"
-                        />
-                        <span
-                            class="invalid-feedback"
-                            role="alert"
-                            v-if="errors.password_confirmation"
-                        >
-                            <strong
-                                v-text="errors.password_confirmation"
-                            ></strong
-                        ></span>
-                    </div>
-                    <div class="form__desc">
+                    <div class="form-desc">
                         登録することで、
-                        <router-link to="/terms" class="form__link"
+                        <router-link to="/terms" class="form-desc__link"
                             >利用規約</router-link
                         >と
-                        <router-link to="/privacy" class="form__link"
+                        <router-link to="/privacy" class="form-desc__link"
                             >プライバシーポリシー</router-link
                         >
                         に同意したものとみなされます。
                     </div>
-                    <button type="submit" class="btn-default btn-default--mail">
-                        新規登録
-                    </button>
-                </form>
-                <div class="social-form">
-                    <p class="social-form__deco">または</p>
-                    <a
-                        class="btn-default btn-default--facebook"
-                        href="/login/facebook"
-                    >
-                        <img
-                            :src="'/img/facebook-logo.svg'"
-                            alt="Facebook"
-                            class="btn-default__logo"
-                        />Facebookで登録
-                    </a>
-                    <a
-                        class="btn-default btn-default--google"
-                        href="/login/google"
-                    >
-                        <img
-                            :src="'/img/google-logo.svg'"
-                            alt="Google"
-                            class="btn-default__logo"
-                        />Googleで登録
-                    </a>
                 </div>
             </div>
         </div>
+        <login-footer></login-footer>
     </div>
 </template>
 
-<script>
-import { mapActions } from "vuex";
+<style>
+.form-btn__list {
+    margin: 0 0 40px;
+}
 
-export default {
-    data() {
-        return {
-            form: {
-                name: "",
-                email: "",
-                password: "",
-                password_confirmation: ""
-            },
-            errors: {}
-        };
-    },
-    methods: {
-        ...mapActions({
-            register: "auth/register"
-        }),
-        signup() {
-            this.errors = {};
-            var self = this;
-            this.register(this.form)
-                .then(() => {
-                    this.$router.replace({
-                        name: "verifyemail"
-                    });
-                })
-                .catch(error => {
-                    var errors = {};
-                    for (var key in error.response.data.errors) {
-                        errors[key] = error.response.data.errors[key][0];
-                    }
-                    self.errors = errors;
-                });
-        },
-        onChangeName: function(value) {
-            if (value === "") {
-                this.errors.name = "氏名は必ず指定してください。";
-            } else {
-                this.errors.name = "";
-            }
-        },
-        onChangeEmail: function(value) {
-            const emailres = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-            if (value === "") {
-                this.errors.email = "メールアドレスは必須です。";
-            } else if (emailres.test(value)) {
-                this.errors.email = "";
-            } else {
-                this.errors.email = "";
-            }
-        },
-        onChangePassword: function(value) {
-            const passwordres = /^[a-zA-Z0-9]+$/;
-            if (value === "") {
-                this.errors.password = "パスワードは必ず指定してください。";
-            } else if (passwordres.test(value)) {
-                this.error.username = "パスワードは半角英数のみです。";
-            } else {
-                this.errors.password = "";
-            }
-        }
-    }
-};
-</script>
+.form-desc {
+    color: #888;
+}
+</style>
